d481a686b773a768c24bc8a9163dd2c87e1f0664
Cassandra Tam <cassandra.wx.tam@gmail.com>
2021-09-19 21:19:53 +1000

    chore: fix linting for src files

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import Dropzone from 'react-dropzone'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Dropzone from 'react-dropzone';
+import './App.css';
 
-const apiUrl = 'http://localhost:3001'
+const apiUrl = 'http://localhost:3001';
 
 type Booking = {
   time: number;
   duration: number;
   userId: string;
-}
+};
 
 export const App = () => {
-  const [bookings, setBookings] = useState<Booking[]>([])
+  const [bookings, setBookings] = useState<Booking[]>([]);
 
   useEffect(() => {
     fetch(`${apiUrl}/bookings`)
       .then((response) => response.json())
-      .then(setBookings)
-  }, [])
+      .then(setBookings);
+  }, []);
 
   const onDrop = (files: File[]) => {
-    console.log(files)
-  }
+    console.log(files);
+  };
 
   return (
-    <div className='App'>
-      <div className='App-header'>
-        <Dropzone accept='.csv' onDrop={onDrop}>
-        {({getRootProps, getInputProps}) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drop some files here, or click to select files</p>
-            </div>
-          </section>
-        )}
+    <div className="App">
+      <div className="App-header">
+        <Dropzone accept=".csv" onDrop={onDrop}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
         </Dropzone>
       </div>
-      <div className='App-main'>
+      <div className="App-main">
         <p>Existing bookings:</p>
         {bookings.map((booking, i) => {
-          const date = new Date(booking.time)
-          const duration = booking.duration / (60 * 1000)
+          const date = new Date(booking.time);
+          const duration = booking.duration / (60 * 1000);
           return (
-            <p key={i} className='App-booking'>
-              <span className='App-booking-time'>{date.toString()}</span>
-              <span className='App-booking-duration'>
-                {duration.toFixed(1)}
-              </span>
-              <span className='App-booking-user'>{booking.userId}</span>
+            <p key={i} className="App-booking">
+              <span className="App-booking-time">{date.toString()}</span>
+              <span className="App-booking-duration">{duration.toFixed(1)}</span>
+              <span className="App-booking-user">{booking.userId}</span>
             </p>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
